440403fbe5ac235f6be16c5327b168f22de82783
ashwink2718 <ashwink2718@gmail.com>
2018-08-15 23:26:16 +0200

    Index created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all  
+  end
+
   def new
     @article = Article.new
   end  
@@ -7,7 +11,6 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
-
 
   def create
     @article = Article.new(article_params) 
@@ -23,17 +26,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully edited."
-      redirect_to article_path(@article)
+      redirect_to articles_path
     else
       render 'edit'
     end 
   end
 
-
   def show
     @article = Article.find(params[:id])
   end
-
 
   private
   def article_params
